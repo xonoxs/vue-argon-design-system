8ce2cf78b0f9914384cde178e117fecb6fb5af8f
xonoxs <135383475+xonoxs@users.noreply.github.com>
2023-06-03 12:20:46 +0200

    Update AppHeader.vue

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -56,6 +56,7 @@
                     <router-link to="/profile" class="dropdown-item">Profile</router-link>
                     <router-link to="/login" class="dropdown-item">Login</router-link>
                     <router-link to="/register" class="dropdown-item">Register</router-link>
+                    <router-link to="/" class="dropdown-item">Testing</router-link>
                 </base-dropdown>
             </ul>
             <ul class="navbar-nav align-items-lg-center ml-lg-auto">
