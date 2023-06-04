928572a44a926a530b196096b6f5c3f947995a8b
xonoxs <135383475+xonoxs@users.noreply.github.com>
2023-06-04 18:02:00 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Landing from "./views/Landing.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
 import Profile from "./views/Profile.vue";
+import Profile from "./views/Testing.vue";
 
 Vue.use(Router);
 
